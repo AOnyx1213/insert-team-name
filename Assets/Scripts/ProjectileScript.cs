d0637b7069cb17d5d9ab097e54ef605f514624f6
Kenneth Cole <kenneth.cole@selu.edu>
2022-11-05 10:41:01 -0500

    Before all merge

--- Assets/Scripts/ProjectileScript.cs
+++ Assets/Scripts/ProjectileScript.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ProjectileScript : MonoBehaviour
 {
     public float Speed = 2.5f;
+    public SpriteRenderer projectileTest;
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(projectileTest.flipX);
         transform.position += transform.right * Time.deltaTime * Speed;
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
